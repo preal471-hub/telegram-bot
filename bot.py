1a4f48a1eca3e7e045f65c5d3963899c5b1c31dd
preal471-hub <preal471@gmail.com>
2026-02-20 20:23:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,25 @@ import telebot
 from telebot.types import ChatJoinRequest
 import json
 import time
+import os
+from flask import Flask
+import threading
 
-TOKEN = "8266089449:AAEJ35ONfsuI2glgRQW_WVwfmGguDp0MEfU"
-CHANNEL_ID = -1002280087377   # we will add later
+TOKEN = os.environ.get("8266089449:AAEUivdYzOEhVbjAzC4UBVry-Z3HVKIurMo")   # IMPORTANT for Render
+CHANNEL_ID = -1002280087377
 
 bot = telebot.TeleBot(TOKEN)
+
+# Dummy Flask server (for Render Free Web Service)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 # Save users
 def save_user(user_id):
@@ -21,35 +35,28 @@ def save_user(user_id):
         with open("users.json","w") as f:
             json.dump(users,f)
 
-# START command
 @bot.message_handler(commands=['start'])
 def start(message):
     save_user(message.chat.id)
     bot.send_message(message.chat.id,
     "👋 Welcome Trader!\nClick below to join OUR Channel 💎")
 
-# Auto approve join request
 @bot.chat_join_request_handler()
 def approve_join_request(join_request: ChatJoinRequest):
     user_id = join_request.from_user.id
-
-    # Save user automatically when they request to join channel
     save_user(user_id)
 
-    # Approve join request
     bot.approve_chat_join_request(
         join_request.chat.id,
         user_id
     )
 
-    # Send welcome DM after approval
     try:
         bot.send_message(user_id,
         "🎉 You are approved in OUR Channel!\n\nStay ready for premium trades 🚀")
     except:
         pass
 
-# Broadcast command
 @bot.message_handler(commands=['broadcast'])
 def broadcast(message):
     msg = message.text.replace("/broadcast ","")
@@ -66,4 +73,6 @@ def broadcast(message):
 
     bot.reply_to(message,"Broadcast sent!")
 
-bot.infinity_polling()
+if name == "__main__":
+    threading.Thread(target=run_web).start()
+    bot.infinity_polling()
